a1bc6611cfaa40f2840e4edaf557c5f987bd7f58
Chalita-Kcs <chalitaseangproh@gmail.com>
2016-09-25 23:59:37 +0700

    case 1 (0,0)

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,6 +1,24 @@
 
+function teampoint(SA,SB)
+{
+    var str = '';
+
+    if(SA === 0 )
+      {
+          str += 'Love-';
+      }
+    if( SB === 0)
+      {
+          str += 'Love';
+      }
+    return str;
+  }
+
 describe ( "Tennis Game" ,function()
 {
-    
+  it('Should be "Love-Love" when start game'),function()
+    {
+        var score = teampoint(0,0);
+    }
 
 });
