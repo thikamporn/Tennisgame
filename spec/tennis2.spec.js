12789afa3b80a893c23e656763696f0aa22896d2
Thikamporn Viriyapongpringam <ploythikamporn@gmail.com>
2016-09-28 19:33:26 +0700

    A win

--- spec/tennis2.spec.js
+++ spec/tennis2.spec.js
@@ -14,6 +14,30 @@ function tennispoint()
       {
           str += 'Love';
       }
+    if(this.playAGetscore === 15)
+     {
+          str += 'fifteen-';
+     }
+    if(this.playBGetscore === 15)
+     {
+          str += 'fifteen';
+     }
+    if(this.playAGetscore === 30)
+     {
+          str += 'thirty-';
+     }
+    if(this.playBGetscore === 30)
+     {
+          str += 'thirty' ;
+     }
+    if(this.playAGetscore === 40)
+    {
+          str += 'A Win the game';
+    }
+    if(this.playBGetscore === 40)
+    {
+         str += 'B Win the game' ;
+    }
       return str;
   };
 }
@@ -26,4 +50,25 @@ function tennispoint()
 	      point.playBGetscore = 0;
 	      expect(point.echo()).toEqual('Love-Love');
 	  });
+    it('should  be  "fifteen = Love" when A=15 and B=0',function()
+    {
+       var point = new tennispoint();
+        point.playAGetscore = 15;
+        point.playBGetscore = 0;
+        expect(point.echo()).toEqual('fifteen-Love');
+    });
+    it('should  be  "thirty = Love" when A=30 and B=0',function()
+    {
+       var point = new tennispoint();
+        point.playAGetscore = 30;
+        point.playBGetscore = 0;
+        expect(point.echo()).toEqual('thirty-Love');
+    });
+    it('should  be  "A won the game" when teamA have score 40',function()
+    {
+       var point = new tennispoint();
+        point.playAGetscore = 40;
+        point.playBGetscore = 0;
+        expect(point.echo()).toEqual('A won the game');
+    });
   });
